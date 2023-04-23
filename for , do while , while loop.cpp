3fde6c2cc4220f9d2001172cb219c24056d17c64
Markosfikadu <130024416+Markosfikadu@users.noreply.github.com>
2023-04-23 15:38:30 +0300

    Add files via upload

---
+++ for , do while , while loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// for loop 
+int i;
+for(int i=0;i<=8;i++)
+cout<<i<<endl;
+// while loop 
+int f;
+while(f<=4){
+cout <<f<<endl;
+f++;
+}
+// do while loop
+int k=0;
+do{
+cout <<k<<endl ;
+k++;
+}while(k<=6);
+
+
+return 0;
+}
