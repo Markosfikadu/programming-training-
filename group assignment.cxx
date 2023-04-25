6eed08c228f05f94c362559f8d678e6812bfa9a9
Markosfikadu <130024416+Markosfikadu@users.noreply.github.com>
2023-04-25 10:02:21 +0300

    Add files via upload

---
+++ group assignment.cxx
@@ -0,0 +1,68 @@
+//group assignment
+  /*1, Eliyas jemal,
+      2,muaz husen,
+      3, Markos Fikadu
+      4, Daniel Fantahun   */ 
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+// variable declaration 
+	int x=5;
+	int y=4;
+	int sum=x+y;
+	cout<<sum;
+//identifier
+	int Bag=5;
+	cout<<Bag;
+// constant 
+cout<<"the sum is"<<sum;cout<<"the sum is"<<sum;cout<<"the sum is"<<sum;	const float pi=3.14;
+	const int sec=60;
+	const  float sum=pi+ sec;
+	cout<<sum;
+// basic C++
+int myNum = 5;              
+  float myFloatNum = 44.75;     
+  double myDoubleNum = 0.98;   
+  char myLetter = 's';         
+  bool myBoolean = true;     
+  string myString = "I Love you";   
+  cout << "int: " << myNum << "\n";
+  cout << "float: " << myFloatNum << "\n";
+  cout << "double: " << myDoubleNum << "\n";
+  cout << "char: " << myLetter << "\n";
+  cout << "bool: " << myBoolean << "\n";
+  cout << "string: " << myString << "\n";
+ // if else
+  int a;
+ 	cout<<"enter the number"<<endl;	cin>>a;
+ 		if (a<10){
+ 				cout<<"aa"<<endl;
+ 			} 
+ 			 else if (a<20){ //else if statment
+ 			   cout<<"bb"<<endl;  } 
+ 			    else if(a<30){  
+ 			     cout <<"cc"<<endl;
+ 			       }
+ 			         else{  
+ 			         	cout<<"error"<<endl;
+ 			         }
+ // nested if
+ 			int a,b,c;
+				cout<<"enter the numbers"<<endl;	cin>>a>>b>>c;
+					if(a>b){
+								if(b>c)		cout<<"jemal"<<endl;
+						
+					 else  cout<<"notcorrect"<<endl
+				;	}
+// for loop
+		for(int b=0;b<4;b++)
+			cout<<"eliyas"<<endl;}
+//string
+	string mytecher = "osam ";
+ 	cout<<mytecher ;
+ }
+
+
+	return 0;
+}
